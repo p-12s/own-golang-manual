e5e74f2d1f43a81a19e9ce0f6b9707694b386023
p-12s <mental.notes@yandex.ru>
2021-05-22 19:59:52 +0300

    Добавлено описание структуры коммерческих больших проектов

---
+++ 5-project-structure-example/cmd/README.md
@@ -0,0 +1,7 @@
+# `cmd`  
+  
+Запуск компиляции разных проектов:  
+```
+go build -o app cmd/app/main.go
+go build -o parser cmd/parser/main.go
+```
---
+++ 5-project-structure-example/cmd/app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//import "github.com/zhashkevych/creatly-backend/internal/app"
+
+const configsDir = "configs"
+
+func main() {
+	fmt.Println("App run")
+	// app.Run(configsDir)
+}
