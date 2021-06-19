f585c90a7de145247cbd5959f9934b7562810ec1
p-12s <mental.notes@yandex.ru>
2021-06-19 18:50:19 +0300

    Добавлено сохранение записи в таблицу блогов: MongoDB + gRPC

--- 8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/client/client.go
+++ 8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/client/client.go
@@ -1,1 +1,51 @@
-package client
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/p-12s/own-golang-manual/8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+)
+
+func main() {
+	fmt.Println("Blog client run")
+
+	isTls := false
+	opts := grpc.WithInsecure()
+	if isTls {
+		creds, err := credentials.NewClientTLSFromFile("../ssl/ca.crt", "")
+		if err != nil {
+			log.Fatalf("could not read cert files: %v", err)
+			return
+		}
+		opts = grpc.WithTransportCredentials(creds)
+	}
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect %v", err)
+	}
+	defer cc.Close()
+
+	c := pb.NewBlogServiceClient(cc)
+
+	doUnary(c)
+}
+
+func doUnary(c pb.BlogServiceClient) {
+	fmt.Println("send blog from client")
+	blog := &pb.Blog{
+		AuthorId: "Mister",
+		Title:    "My title",
+		Content:  "My content",
+	}
+	res, err := c.CreateBlog(context.Background(), &pb.CreateBlogRequest{
+		Blog: blog,
+	})
+	if err != nil {
+		log.Fatalf("error while calling CreateBlog %v\n", err)
+	}
+	log.Printf("\nblog has been created:\n%v\n", res.GetBlog())
+}
--- 8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/server/server.go
+++ 8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/server/server.go
@@ -1,18 +1,87 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/p-12s/own-golang-manual/8-protobuf-grpc/udemy-protocol-buffers-3/05-blog-mongodb/pb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 )
 
 type server struct{}
 
+func (*server) CreateBlog(ctx context.Context, req *pb.CreateBlogRequest) (*pb.CreateBlogResponse, error) {
+	blog := req.GetBlog()
+	data := blogItem{
+		AuthorId: blog.GetAuthorId(),
+		Title:    blog.GetTitle(),
+		Content:  blog.GetContent(),
+	}
+	res, err := collection.InsertOne(context.Background(), data)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("internal error: %v", err),
+		)
+	}
+
+	objId, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("can't convert to objId: %v", err),
+		)
+	}
+
+	return &pb.CreateBlogResponse{
+		Blog: &pb.Blog{
+			Id:       objId.Hex(),
+			AuthorId: blog.GetAuthorId(),
+			Title:    blog.GetTitle(),
+			Content:  blog.GetContent(),
+		},
+	}, nil
+}
+
+var collection *mongo.Collection
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorId string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
 func main() {
+	// if we crashed the code, we get the file name and line
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	fmt.Println("Blog service started")
+
+	ctx := context.Background()
+
+	fmt.Println("DB connect")
+	// uri := "mongodb://mongos0.example.com:27017,mongos1.example.com:27017/"
+	var uri = "mongodb://localhost:27017"
+	clientOpts := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		log.Fatalf("mongodb connect error: %v", err)
+	}
+	defer func() { _ = client.Disconnect(ctx) }()
+
+	collection = client.Database("mydb").Collection("blog")
+
+	fmt.Println("OK")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -33,7 +102,23 @@ func main() {
 	s := grpc.NewServer(opts...)
 	pb.RegisterBlogServiceServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve %v", err)
-	}
+	go func() {
+		fmt.Println("Server start")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve %v", err)
+		}
+	}()
+
+	// Ctr+C waiting to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// block until a signal is received
+	<-ch
+	fmt.Println("Server trying to stop")
+	s.Stop()
+	lis.Close()
+	fmt.Println("DB trying to stop")
+	client.Disconnect(context.TODO())
+	fmt.Println("Server and DB stopped")
 }
