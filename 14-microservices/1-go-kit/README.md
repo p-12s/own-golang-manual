135757c2490231d3e36ac3f3596f0101b8db1e97
p-12s <mental.notes@yandex.ru>
2021-07-09 16:37:19 +0300

    Доабвлен пример сервиса с go-kit, go-chi, mongo+reids

---
+++ 14-microservices/1-go-kit/README.md
@@ -0,0 +1,3 @@
+# Hexagonal-architecture: go-kit + go-chi + mongo+redis
+
+https://github.com/tensor-programming/hex-microservice/tree/part-3
--- 14-microservices/1-go-kit/account/repo.go
+++ 14-microservices/1-go-kit/account/repo.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"github.com/go-kit/kit/log"
@@ -18,4 +19,30 @@ func NewRepo(db *sql.DB, logger log.Logger) *repo {
 		db:     db,
 		logger: log.With(logger, "repo", "sql"),
 	}
+}
+
+func (repo *repo) CreateUser(ctx context.Context, user User) error {
+	sql := `
+		INSERT INTO users (id, email, password)
+		VALUES ($1, $2, $3)`
+
+	if user.Email == "" || user.Password == "" {
+		return RepoErr
+	}
+
+	_, err := repo.db.ExecContext(ctx, sql, user.ID, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *repo) GetUser(ctx context.Context, id string) (string, error) {
+	var email string
+	err := repo.db.QueryRow("SELECT email FROM users WHERE id=$1", id).Scan(&email)
+	if err != nil {
+		return "", RepoErr
+	}
+
+	return email, nil
 }
